ae6349289779882528abd18b1454be223ca93db6
Amroo Salout <a.salout@dce.sa>
2019-01-10 15:52:42 +0200

    updates for package

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import PropTypes from 'prop-types'
 
 export default class TreeComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string.isRequired
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
+    const { text } = this.props
     return (
       <div>
         {text}
