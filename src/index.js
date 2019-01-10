564703bff6be7ea1849b0b85466e3514e3ff4151
Amroo Salout <a.salout@dce.sa>
2019-01-10 14:45:01 +0200

    updates for publish

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import styles from './styles.css'
 
 export default class TreeComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
+    text: ''
   }
 
   render() {
